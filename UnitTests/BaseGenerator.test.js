01cb8db45b528e2c77dd9df2ce321546bf6f4b82
13MykytaSh <nik.shendrik@gmail.com>
2024-12-07 23:16:54 +0200

    BaseGenerator comments

--- UnitTests/BaseGenerator.test.js
+++ UnitTests/BaseGenerator.test.js
@@ -1,5 +1,11 @@
 import { BaseGenerator } from '../Generators/BaseGenerator.js';
 
+// The BaseGenerator class is tested.
+// Checks that BaseGenerator is created without errors.
+// Checks that the Generate method is available on the class instance.
+// Checks that calling Generate does not throw exception.
+// Checks that the base method does not return anything.
+
 describe('BaseGenerator', () => {
 
     let generator;
@@ -14,14 +20,16 @@ describe('BaseGenerator', () => {
         jest.clearAllMocks();
     });
 
+    // Checks that the Generate method is available on the class instance.
     test('Should have a Generate method', () => {
         expect(typeof generator.Generate).toBe('function');
     });
 
+    // Checks that calling Generate does not throw exception.
     test('Generate method should not throw by default', () => {
         expect(() => generator.Generate({})).not.toThrow();
     });
-
+    // Checks that the base method does not return anything.
     test('Generate method should not return any value by default', () => {
         const result = generator.Generate({});
         expect(result).toBeUndefined();
