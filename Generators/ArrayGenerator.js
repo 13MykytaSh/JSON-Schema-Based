a194e8f039aaeca5a454bd02a5ed6fa355f8ecef
13MykytaSh <nik.shendrik@gmail.com>
2024-12-07 19:06:39 +0200

    improvements

--- Generators/ArrayGenerator.js
+++ Generators/ArrayGenerator.js
@@ -34,7 +34,7 @@ export class ArrayGenerator extends BaseGenerator {
      * @param {number} [schema.minItems=MIN_ITEMS] - The minimum number of items in the array.
      * @param {number} [schema.maxItems=MAX_ITEMS] - The maximum number of items in the array.
      * @param {boolean} [schema.uniqueItems=UNIQUE_ITEMS] - Whether the items in the array should be unique.
-     * @returns {Array} - The generated array of items.
+     * @returns {Array} - The generated array of items, if the input parameters are incorrect, an empty array will be returned
      * 
      * @example
      * const schema = {
@@ -49,6 +49,10 @@ export class ArrayGenerator extends BaseGenerator {
     Generate(schema) {
         //Get schema from items
         const itemsSchema = schema.items;
+        //Checking the validity of the input data.
+        if (!itemsSchema || typeof itemsSchema !== 'object' || !itemsSchema.type) {
+            return [];
+        }
         //Get minimum number of items in the array.
         const minItems = GetValidValueBetween(schema.minItems, MIN_ITEMS);
         //Get maximum number of items in the array.
@@ -57,19 +61,40 @@ export class ArrayGenerator extends BaseGenerator {
         const uniqueItems = GetValidValueBetween(schema.uniqueItems, UNIQUE_ITEMS);
         //Create random length between the minimum and maximum.
         const length = Math.floor(GetRandomNumberBetween(minItems, maxItems));
+
+        //Incorrect input data validations. It's not good to adapt, it's better for the user to change the scheme than for us to change the results.
+
+        //Can't genere. It is impossible to generate an array with more than two elements with a data type that has only 2 options.
+        if (itemsSchema.type === 'boolean' && uniqueItems && length > 2)
+            return [];
+
+        //In general MinItems < 0 is not normal. in case with random generation length can be > 0
+        //MinItems is set to <= 0 and length can be <=0 or parameters were specified incorrectly to generate the length.
+        if (length <=0 || length === null)
+            return [];
+
+        //Unable to generate unique integers in low range.
+        if (itemsSchema.type === 'integer' && uniqueItems) {
+            const range = itemsSchema.maximum - itemsSchema.minimum + 1;
+            if (length > range) {
+                return [];
+            }
+        }
+
         //Create Set for storing the obtained items.
         const generatedItems = new Set();
         //Create array for result
         const result = [];
 
-        //Use GenerationCore to generate nested properties
+        //Use GenerationCore to generate nested properties.
         const generator = new GenerationCore();
-
-        while (generatedItems.size < length) {
+        //Used to count the number of elements created to meet the condition for exiting the cycle
+        let itemsCunter = 0;
+        while (itemsCunter < length) {
             //Call item generation
             const item = generator.StartGeneration(itemsSchema);
             if (uniqueItems) {
-                //If uniqueItems make JSON from item to filter out duplicates
+                //If uniqueItems make JSON from item to filter out duplicates.
                 const itemStr = JSON.stringify(item);
                 if (generatedItems.has(itemStr))
                     continue;
@@ -77,6 +102,7 @@ export class ArrayGenerator extends BaseGenerator {
                     generatedItems.add(itemStr);
             }
             result.push(item);
+            itemsCunter++;
         }
 
         return result;
