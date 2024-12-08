cd9484135051bf1813dfbb5e72d8ed619d24b3ad
13MykytaSh <nik.shendrik@gmail.com>
2024-12-08 02:37:18 +0200

    IntegerGenerator comments

--- UnitTests/IntegerGenerator.test.js
+++ UnitTests/IntegerGenerator.test.js
@@ -72,8 +72,8 @@ describe('IntegerGenerator', () => {
         expect(resultS).toBeNull();
     });
 
-    // Test correct integer generation with the same parameters minimum and maximum. Expected result is null
-    test('should return null when minimum and maximum are equal', () => {
+    // Test correct integer generation with the same parameters minimum and maximum. Expected result is value equal to minimum and maximum
+    test('should return value equal to minimum and maximum when minimum and maximum are equal', () => {
         // Generate a number
         const schema = { minimum: 30, maximum: 30 };
         const result = generator.Generate(schema);
